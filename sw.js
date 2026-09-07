c6e79c3d938770fa53401f257c71485c2ecadcfe
kuttiboomofficial-beep <kuttiboomofficial@gmail.com>
2026-09-07 15:18:45 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "shree-ji-connect-v72";
+const CACHE = "shree-ji-connect-v7-1";
 const ASSETS = ["./", "./index.html", "./manifest.json"];
 
 self.addEventListener("install", event => {

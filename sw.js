e3726c251dbe9836daf996b75a3c574e02871518
kuttiboomofficial-beep <kuttiboomofficial@gmail.com>
2026-09-08 12:16:00 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,30 +1,9 @@
-const CACHE = "shree-ji-connect-v7-1";
-const ASSETS = ["./", "./index.html", "./manifest.json"];
-
-self.addEventListener("install", event => {
-  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
-  self.skipWaiting();
-});
-
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
-  );
-  self.clients.claim();
-});
-
-self.addEventListener("fetch", event => {
-  if (event.request.method !== "GET") return;
-  const url = new URL(event.request.url);
-  if (url.pathname.endsWith("/index.html") || url.pathname.endsWith("/")) {
-    event.respondWith(fetch(event.request).then(response => {
-      const copy=response.clone();
-      caches.open(CACHE).then(cache=>cache.put(event.request,copy));
-      return response;
-    }).catch(()=>caches.match(event.request).then(r=>r||caches.match("./index.html"))));
-    return;
-  }
-  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).then(response=>{
-    const copy=response.clone(); caches.open(CACHE).then(cache=>cache.put(event.request,copy)); return response;
-  }).catch(()=>caches.match("./index.html"))));
+const CACHE='sjc-offline-v1';
+const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
+self.addEventListener('fetch',e=>{
+  const r=e.request;
+  if(r.method!=='GET')return;
+  e.respondWith(caches.match(r).then(cached=>cached||fetch(r).then(res=>{const copy=res.clone();if(new URL(r.url).origin===location.origin){caches.open(CACHE).then(c=>c.put(r,copy));}return res;}).catch(()=>caches.match('./index.html'))));
 });
